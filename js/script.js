ab182d11c6b6ca9606ef6cec94a3dd7c966c4584
Sierra Morgan <sierramorgan07@gmail.com>
2015-06-27 22:06:39 -0700

    hmm

--- js/script.js
+++ js/script.js
@@ -19,7 +19,92 @@ var data = d3.csv("data.csv", function(data) {
 		    .data(data)
 		    .enter()
 		    .append("div")
-		    .text( function (d) { return d.date; });       
+		    .text( function (d) { return d.date; }); 
+
+
+		var fish1 = d3.select("body").selectAll("#fish1")
+		    .data(data)
+		    .enter()
+		    .append("p")
+		    .text( function (d) { return d.allChinook; });
+
+
+		var fish2 = d3.select("body").selectAll("#fish2")
+		    .data(data)
+		    .enter()
+		    .append("p")
+		    .text( function (d) { return d.adultChinook; });
+
+
+		var fish3 = d3.select("body").selectAll("#fish3")
+		    .data(data)
+		    .enter()
+		    .append("p")
+		    .text( function (d) { return d.jackChinook; }); 
+
+
+		var fish4 = d3.select("body").selectAll("#fish4")
+		    .data(data)
+		    .enter()
+		    .append("p")
+		    .text( function (d) { return d.allSteelhead; }); 
+
+
+		var fish5 = d3.select("body").selectAll("#fish5")
+		    .data(data)
+		    .enter()
+		    .append("p")
+		    .text( function (d) { return d.clippedChinook; }); 
+
+
+		var fish6 = d3.select("body").selectAll("#fish6")
+		    .data(data)
+		    .enter()
+		    .append("p")
+		    .text( function (d) { return d.unclippedChinook; }); 
+
+
+		var fish7 = d3.select("body").selectAll("#fish7")
+		    .data(data)
+		    .enter()
+		    .append("p")
+		    .text( function (d) { return d.allCoho; }); 
+
+
+		var fish8 = d3.select("body").selectAll("#fish8")
+		    .data(data)
+		    .enter()
+		    .append("p")
+		    .text( function (d) { return d.adultCoho; });
+
+
+		var fish9 = d3.select("body").selectAll("#fish9")
+		    .data(data)
+		    .enter()
+		    .append("p")
+		    .text( function (d) { return d.jackCoho; });
+
+
+		var fish10 = d3.select("body").selectAll("#fish10")
+		    .data(data)
+		    .enter()
+		    .append("p")
+		    .text( function (d) { return d.sockeye; }); 
+
+
+		var fish11 = d3.select("body").selectAll("#fish11")
+		    .data(data)
+		    .enter()
+		    .append("p")
+		    .text( function (d) { return d.chum; });
+		    
+
+		var fish12 = d3.select("body").selectAll("#fish12")
+		    .data(data)
+		    .enter()
+		    .append("p")
+		    .text( function (d) { return d.pink; });    
+
 
 	});
 
