174cec64dcd8aa342b4d5417fad206ab9c442567
Sierra Morgan <sierramorgan07@gmail.com>
2015-06-27 21:36:38 -0700

    returning dates

--- js/script.js
+++ js/script.js
@@ -1,26 +1,32 @@
 var data = d3.csv("data.csv", function(data) {
 
-	data.forEach(function(d) {
-     	d.date = d.date;
-     	d.allChinook = +d.allChinook;
-     	d.adultChinook = +d.adultChinook;
-     	d.jackChinook = +d.jackChinook;
-     	d.allSteelhead = +d.allSteelhead;
-     	d.clippedSteelhead = +d.clippedSteelhead;
-     	d.unclippedSteelhead = +d.unclippedSteelhead;
-     	d.allCoho = +d.allCoho;
-     	d.adultCoho = +d.adultCoho;
-     	d.jackCoho = +d.jackCoho;
-     	d.sockeye = +d.sockeye;
-     	d.chum = +d.chum;
-     	d.pink = +d.pink;
+ var each = data.forEach(function(d) {
+	     	d.date = d.date;
+	     	d.allChinook = +d.allChinook;
+	     	d.adultChinook = +d.adultChinook;
+	     	d.jackChinook = +d.jackChinook;
+	     	d.allSteelhead = +d.allSteelhead;
+	     	d.clippedSteelhead = +d.clippedSteelhead;
+	     	d.unclippedSteelhead = +d.unclippedSteelhead;
+	     	d.allCoho = +d.allCoho;
+	     	d.adultCoho = +d.adultCoho;
+	     	d.jackCoho = +d.jackCoho;
+	     	d.sockeye = +d.sockeye;
+	     	d.chum = +d.chum;
+	     	d.pink = +d.pink;
 
-
-    var output = d3.select("body").selectAll("div")
-		.data(d);
+     	var thedate = d3.select("body").selectAll("div")
+		    .data(data)
+		    .enter()
+		    .append("div")
+		    .text( function (d) { return d.date; });       
 
 	});
 
    console.log(data[0])
+
 });
+
+
+
 
