fd80f8cc602097c27272d3fb59ab3ab2645920af
Sierra Morgan <sierramorgan07@gmail.com>
2015-06-27 20:05:26 -0700

    get data

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
-var main = function () {
-    "use strict";
+var data = d3.csv("data.csv", function(data) {
 
-    var pirates = [];
+	data.forEach(function(d) {
+     	d.date = d.date;
+     	d.allChinook = +d.allChinook;
+     	d.adultChinook = +d.adultChinook;
+     	d.jackChinook = +d.jackChinook;
+     	d.allSteelhead = +d.allSteelhead;
+     	d.clippedSteelhead = +d.clippedSteelhead;
+     	d.unclippedSteelhead = +d.unclippedSteelhead;
+     	d.allCoho = +d.allCoho;
+     	d.adultCoho = +d.adultCoho;
+     	d.jackCoho = +d.jackCoho;
+     	d.sockeye = +d.sockeye;
+     	d.chum = +d.chum;
+     	d.pink = +d.pink;
 
-	// Assuming you have referenced jQuery
-	$(function() {
-	    $.get("http://www.fpc.org/WebForm3.5/GetAdultCounts.aspx?id=1", function(crewResponse) {
-	        pirates = crewResponse.pirates;
-	        alert("Load was performed.");
-	    });
+
+    var output = d3.select("body").selectAll("div")
+		.data(d);
+
 	});
 
-};
+   console.log(data[0])
+});
 
-$(document).ready(main);
