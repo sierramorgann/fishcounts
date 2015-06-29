d261710b551e100a39ae03fdcee289dbff234260
Sierra Morgan <sierramorgan07@gmail.com>
2015-06-29 12:23:17 -0700

    so far

--- js/script.js
+++ js/script.js
@@ -1,40 +1,6 @@
 var data = d3.csv("data.csv", function(data) {
+
 var axis = data.forEach(function() {
-        var margin = {
-                top: 10,
-                right: 10,
-                bottom: 20,
-                left: 5},
-
-            width = 750 - margin.left - margin.right,
-            height = 30 - margin.top - margin.bottom;
-
-        var x = d3.time.scale()
-            .domain([new Date(2015, 0, 1), new Date(2015, 11, 31)])
-            .range([0, width]);
-
-        var xAxis = d3.svg.axis()
-            .scale(x)
-            .orient("bottom")
-            .ticks(d3.time.months)
-            .tickSize(12, 0)
-            .tickFormat(d3.time.format("%B"));
-
-        var svg = d3.select("body").append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-        svg.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")")
-            .call(xAxis)
-            .selectAll(".tick text")
-            .style("text-anchor", "start")
-            .attr("x", 6)
-            .attr("y", 6);
-
     });
     var each = data.forEach(function(d) {
         d.date = d.date;
@@ -75,11 +41,47 @@ var axis = data.forEach(function() {
         .style("float", "left")
         .style("padding-left", "1px")
         .style("margin-left", "1px")
-        .style("background-color", "blue")
+        .style("background-color", "rgb(0,127,107)")
         .style("height", function(d) {
             var barHeight = d.allChinook * 0.1;
             return barHeight + "px";
-        });
+        })
+
+        var margin = {
+                top: 10,
+                right: 6,
+                bottom: 20,
+                left: 6},
+
+            width = 300 - margin.left - margin.right,
+            height = 80 - margin.top - margin.bottom;
+
+        var x = d3.time.scale()
+            .domain([new Date(2015, 0, 1), new Date(2015, 11, 31)])
+            .range([width, 0]);
+
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom")
+            .ticks(d3.time.months)
+            .tickSize(12, 0)
+            .tickFormat(d3.time.format("%B"));
+
+        var svg = d3.select("body").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+            .selectAll(".tick text")
+            .style("text-anchor", "start")
+            .attr("x", 6)
+            .attr("y", 6);
+        
 
     var fish2 = d3.select("div", "fish2").selectAll()
         .data(data)
@@ -88,7 +90,7 @@ var axis = data.forEach(function() {
         .style("float", "left")
         .style("padding", "1px")
         .style("margin-left", "1px")
-        .style("background-color", "red")
+        .style("background-color", "rgb(0,255,208)")
         .style("height", function(d) {
             var barHeight = d.adultChinook * 0.1;
             return barHeight + "px";
@@ -102,7 +104,7 @@ var axis = data.forEach(function() {
         .style("float", "left")
         .style("padding", "1px")
         .style("margin-left", "1px")
-        .style("background-color", "green")
+        .style("background-color", "rgb(130,255,232)")
         .style("height", function(d) {
             var barHeight = d.jackChinook * 0.1;
             return barHeight + "px";
@@ -115,7 +117,7 @@ var axis = data.forEach(function() {
         .style("float", "left")
         .style("padding", "1px")
         .style("margin-left", "1px")
-        .style("background-color", "orange")
+        .style("background-color", "rgb(0,191,156)")
         .style("height", function(d) {
             var barHeight = d.allSteelhead * 0.1;
             return barHeight + "px";
@@ -142,7 +144,7 @@ var axis = data.forEach(function() {
         .style("float", "left")
         .style("padding", "1px")
         .style("margin-left", "1px")
-        .style("background-color", "grey")
+        .style("background-color", "rgb(0,64,52")
         .style("height", function(d) {
             var barHeight = d.unclippedSteelhead * 0.1;
             return barHeight + "px";
@@ -169,7 +171,7 @@ var axis = data.forEach(function() {
         .style("float", "left")
         .style("padding", "1px")
         .style("margin-left", "1px")
-        .style("background-color", "black")
+        .style("background-color", "rgb(14,255,233)")
         .style("height", function(d) {
             var barHeight = d.adultCoho * 0.1;
             return barHeight + "px";
@@ -182,7 +184,7 @@ var axis = data.forEach(function() {
         .style("float", "left")
         .style("padding", "1px")
         .style("margin-left", "1px")
-        .style("background-color", "yellow")
+        .style("background-color", "rgb(3,64,58")
         .style("z-index", "10")
         .style("height", function(d) {
             var barHeight = d.jackCoho * 0.1;
@@ -196,7 +198,7 @@ var axis = data.forEach(function() {
         .style("float", "left")
         .style("padding", "1px")
         .style("margin-left", "1px")
-        .style("background-color", "purple")
+        .style("background-color", "rgb(10,191,175)")
         .style("z-index", "100")
         .style("height", function(d) {
             var barHeight = d.sockeye * 0.1;
